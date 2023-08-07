729cb2af30c33e439bf09612ee7e59680631a313
Brent Haag <brenthaag@gmail.com>
2023-08-06 20:11:36 -0500

    update

--- airscape/airscape-whf/src/init.lua
+++ airscape/airscape-whf/src/init.lua
@@ -1,1 +1,35 @@
 
+local capabilities = require "st.capabilities"
+local Driver = require "st.driver"
+local cosock = require "cosock"                                         -- for time only
+local socket = require "cosock.socket"                                  -- for time only
+local log = require "log"
+
+
+local function validate_ipaddress(ip)
+
+    local valid = true
+    
+    if ip then
+      local chunks = {ip:match("^(%d+)%.(%d+)%.(%d+)%.(%d+)$")}
+      if #chunks == 4 then
+        for i, v in pairs(chunks) do
+          if tonumber(v) > 255 then 
+            valid = false
+            break
+          end
+        end
+      else
+        valid = false
+      end
+    else
+      valid = false
+    end
+    
+    if valid then
+      return ip
+    else
+      return nil
+    end
+        
+  end
